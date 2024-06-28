be263090628907a6532a1a79e5cbef6118bbbb24
Rapynets Vasyl <rapinecvasiliy@gmail.com>
2024-06-28 10:02:54 +0300

    split the existing layout into components and connected them

---
+++ public/views/includes/footer.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <div class="footer__wrapper">
+        &copy; <?php echo date("Y-m-d H:i"); echo ' / ' . time(); ?>
+    </div>
+</div>
---
+++ public/views/layout.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NIX Project</title>
+  <link rel="stylesheet" href="assets/css/reset.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+  <div class="wrapper">
+    <header class="header">
+      <?php require_once 'includes/header.php'; ?>
+    </header>
+    <main class="main py-3">
+      <div class="container">Content</div>
+    </main>
+    <footer class="footer bg-dark py-3">
+      <?php require_once 'includes/footer.php'; ?>
+    </footer>
+  </div>
+  
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
